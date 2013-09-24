5ad2593a06ef9f0eec9b31996caf066e99340637
Christopher Vogt <christopher.vogt@epfl.ch>
2013-09-24 00:55:26 -0400

    updated code for JavaOne

--- src/main/scala/LiveDemo.scala
+++ src/main/scala/LiveDemo.scala
@@ -1,11 +1,11 @@
 /**
  * https://github.com/cvogt/slick-presentation/
- * branch: ny-scala-2013
+ * branch: javaone-2013
  */
 import scala.slick.driver.H2Driver.simple._
 import setup._
 
-object NyScala2013LiveDemo extends App {
+object LiveDemo extends App {
   // connection info for a throw-away, in-memory db
   val db = Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver")
 
